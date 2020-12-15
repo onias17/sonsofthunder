af4304d4f867606ff099fa69c9b0e61ca5789861
onias17 <oniasnephiisrael@gmail.com>
2020-12-15 13:03:32 -0800

    appointment model edits

--- main_app/models.py
+++ main_app/models.py
@@ -2,18 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+BUILDING = (
+    ('R', 'Residential'),
+    ('C', 'Commercial'),
+    ('I', 'Industrial'),
+)
+
 class Appointment(models.Model):
     name = models.CharField(max_length=100)
     phone_number = models.BigAutoField
     email = models.EmailField(max_length=100)
-    building_type = models.CharField(max_length=100)
+    building_type = models.CharField(
+        max_length=1,
+        choices=BUILDING,
+    )
     description = models.TextField(max_length=500)
     address = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
     date_created = models.DateTimeField(auto_now_add=True)
-
-appointments = [
-    Appointment('Residential', 'need ligthing fixtures installed in the bathroom', '123 Residential Way', '12/20/20'),
-    Appointment('Commercial', 'need upgraded panels', '456 Commercial Rd', '12/21/20'),
-    Appointment('Industrial', 'need electrical ground work to be completed', '789 Industrial Ave', '12/22/20'),
-];
