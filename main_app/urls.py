2d3a6d67618097d27887338542795eebab7cb950
onias17 <oniasnephiisrael@gmail.com>
2020-12-15 09:43:24 -0800

    created appointments route and Appointment class

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    # path('services/', views.services, name='services'),
+    path('appointments/', views.apointments_index, name='appoitnments'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+class Appointments:
+    def __init__(self, building, description, address, date):
+        self.building = building
+        self.description = description
+        self.address = address
+        self.date = date
+
+appointments = [
+    Appointment('Residential', 'need ligthing fixtures installed in the bathroom', '123 Residential Way', '12/20/20'),
+    Appointment('Copmmercial', 'need upgraded panels', '456 Commercial Rd', '12/21/20'),
+    Appointment('Industrial', 'need electrical ground work to be completed', '789 Industrial Ave', '12/22/20'),
+]
+
+# HOME and ABOUT
 def home(request):
     return HttpResponse('<h1>Sons of Thunder</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+# APPOINTMENTS
+def appointments(request):
+    return render(request, 'appointments/index.html', { 'appointments': appointments })
