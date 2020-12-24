48ce468584ea7187acc814c661981cc8d69f46a9
onias17 <oniasnephiisrael@gmail.com>
2020-12-23 23:06:20 -0800

    settings

--- sot_project/settings.py
+++ sot_project/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
