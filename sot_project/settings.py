873fe5a42d5b2b05e89db79efee3fb663e301ac4
onias17 <oniasnephiisrael@gmail.com>
2020-12-23 23:20:28 -0800

    settings

--- sot_project/settings.py
+++ sot_project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../main_app/static'),
+    os.path.join(BASE_DIR, 'main_app/static'),
 )
 
 
