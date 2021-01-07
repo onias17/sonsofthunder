7cf33c62f6d1ba0e6cd26ae3442ea8a2914e0072
onias17 <oniasnephiisrael@gmail.com>
2021-01-07 14:13:55 -0800

    settings

--- sot_project/settings.py
+++ sot_project/settings.py
@@ -12,24 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 import django_on_heroku
-import dotenv
 
 from pathlib import Path
 
-
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', '0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
